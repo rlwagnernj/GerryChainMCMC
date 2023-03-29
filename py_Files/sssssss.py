58a4c6f3d734883b5cb3460a9c9d7f3e1fcdf859
rlwagnernj <98774877+rlwagnernj@users.noreply.github.com>
2023-03-29 19:56:52 -0400

    Reorganization for readability

--- py_Files/sssssss.py
+++
@@ -1,4 +0,0 @@
-import os 
-
-os.system('start cmd /k C:/Users/rlwagner01/.conda/envs/gerry/python.exe c:/Users/rlwagner01/Documents/GitHub/GerryChainMCMC/py_files/hello.py')
-os.system('taskkill /IM WindowsTerminal.exe')
