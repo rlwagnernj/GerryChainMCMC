a48656d302fbf423b7b2760c40ca1c473cdc74c3
rlwagnernj <98774877+rlwagnernj@users.noreply.github.com>
2023-05-09 20:54:36 -0400

    Chain 2

---
+++ RunGerryChainParallel.py
@@ -0,0 +1,70 @@
+import os 
+import shutil
+from glob import glob
+import time 
+
+RUN   = "C:/Users/rlwagner01/Documents/GitHub/GerryChainMCMC/Output/Chain_05.02/run"   
+NEXT = "C:/Users/rlwagner01/Documents/GitHub/GerryChainMCMC/Output/Chain_05.02/next"    
+USED = "C:/Users/rlwagner01/Documents/GitHub/GerryChainMCMC/Output/Chain_05.02/used" 
+
+FLAG_DATA = 'RunTwo'
+
+branch = 298
+seed = 4000
+parallel_path = 'C:/Users/rlwagner01/Documents/GitHub/GerryChainMCMC/RunGerryChainParallelDefs.py'
+
+while branch < 500:
+
+    num_files = len(os.listdir(RUN))
+
+    if num_files == 0:
+        num_files = 1
+        n=1
+    elif num_files > 10:
+        n=10
+    else:
+        n = num_files
+
+    while num_files > 0: # until RUN is empty
+
+        for i in range(n):
+
+            cmd = 'start cmd /c python '  + parallel_path + ' ' + str(seed) + ' ' + str(i)
+            os.system(cmd)
+
+            seed+=1
+            
+            cmd = 'start cmd /c python ' + parallel_path + ' ' + str(seed) + ' ' + str(i)
+            os.system(cmd)
+
+            seed+=1
+
+            branch +=2
+
+        print("waiting...")
+        time.sleep(3000)
+
+        # move the used files to USED
+        pattern = os.path.join(RUN, FLAG_DATA + '_assignments' + '*')
+        files = glob(pattern)
+        print(str(len(files)) + 'in RUN')
+        if files != []:
+            for i in range(num_files):
+                file = files[i]
+                shutil.move(file, USED)
+        print(str(len(files)) + 'in RsUN')
+        num_files=num_files-10
+        print(branch)
+
+    # move the next files to RUN
+    pattern = os.path.join(NEXT, FLAG_DATA + '_assignments' + '*')
+    files = glob(pattern)
+    for file in files:
+        shutil.move(file, RUN)
+
+
+
+
+"""os.system('C:/Users/rlwagner01/.conda/envs/gerry/python.exe c:/Users/rlwagner01/Documents/GitHub/GerryChainMCMC/hello.py')
+os.system('start cmd /k C:/Users/rlwagner01/.conda/envs/gerry/python.exe c:/Users/rlwagner01/Documents/GitHub/GerryChainMCMC/hello.py')
+os.system('start cmd /k C:/Users/rlwagner01/.conda/envs/gerry/python.exe c:/Users/rlwagner01/Documents/GitHub/GerryChainMCMC/hello.py')"""
